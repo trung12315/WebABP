1cb7041c65ab477314003e08db2411abcaa9b275
trung12315 <trungnguyen01656640761@gmail.com>
2022-03-09 12:40:40 +0700

    abc

---
+++ Acme.BookStore/src/Acme.BookStore.MongoDB/MongoDb/BookStoreMongoDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Acme.BookStore.Authors;
+using Acme.BookStore.Books;
+using MongoDB.Driver;
+using Volo.Abp.Data;
+using Volo.Abp.MongoDB;
+
+namespace Acme.BookStore.MongoDB;
+
+[ConnectionStringName("Default")]
+public class BookStoreMongoDbContext : AbpMongoDbContext
+{
+    public IMongoCollection<Author> Authors => Collection<Author>();
+    public IMongoCollection<Book> Books => Collection<Book>();
+    /* Add mongo collections here. Example:
+     * public IMongoCollection<Question> Questions => Collection<Question>();
+     */
+
+    protected override void CreateModel(IMongoModelBuilder modelBuilder)
+    {
+        base.CreateModel(modelBuilder);
+
+        //builder.Entity<YourEntity>(b =>
+        //{
+        //    //...
+        //});
+    }
+}
---
+++ Acme.BookStore/src/Acme.BookStore.Web/BookStoreWebAutoMapperProfile.cs
@@ -0,0 +1,13 @@
+﻿using Acme.BookStore.Books;
+using AutoMapper;
+
+namespace Acme.BookStore.Web;
+
+public class BookStoreWebAutoMapperProfile : Profile
+{
+    public BookStoreWebAutoMapperProfile()
+    {
+        CreateMap<BookDto, CreateUpdateBookDto>();
+        //Define your AutoMapper configuration here for the Web project.
+    }
+}
